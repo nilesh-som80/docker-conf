a07fed7ea32ac0e8c1ce9b5217cdf7f4185707a5
Nilesh Singh <69386843+nilesh-som80@users.noreply.github.com>
2024-02-09 10:52:22 +0000

    mongodb connection enabled at container level

--- nodeApp/index.js
+++ nodeApp/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const morgan  = require('morgan');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+mongoose.connect("mongodb://root:pass@mongo:27017/").then(()=>{
+    console.log("mongodb connected");
+})
+app.use(morgan('combined'))
 
 app.get('/',(req,res)=>{
     return res.json({message:"hit home page url"})
@@ -9,7 +14,6 @@ app.get('/',(req,res)=>{
 app.get('*',(req,res)=>{
     return res.sendFile(path.join(__dirname,'/public/index.html'))
 })
-
 app.listen(80,()=>{
     console.log("node app started at 80");
 })
