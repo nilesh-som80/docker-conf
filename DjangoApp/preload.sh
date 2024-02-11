ed2425baeef2effc79fcef8f6c48a9c7051fc0f7
Nilesh Sandeep Singh <nileshsomvansi@gmail.com>
2024-02-11 15:47:30 +0530

    static file config for admin panel

--- DjangoApp/preload.sh
+++ DjangoApp/preload.sh
@@ -1,2 +1,11 @@
-python manage.py migrate
+#!/usr/bin bash
+cd /app/DjangoApp
+apk add build-base
+pip3 install -r requirements.txt
+
+pip3 install gunicorn
+
+python3 manage.py migrate
+python3 manage.py collectstatic
+
 gunicorn DjangoApp.wsgi -b 0.0.0.0:80
